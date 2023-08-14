6827ef5120b1e81c157c2b763ef3733774653faf
smile <understandable997@gmail.com>
2023-08-14 11:32:13 +0800

    remove unused code

--- app/src/main/java/com/wzp/mvvm_template/presentation/app/MainActivity.kt
+++ app/src/main/java/com/wzp/mvvm_template/presentation/app/MainActivity.kt
@@ -7,7 +7,6 @@ import com.wzp.mvvm_template.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
         ActivityMainBinding.inflate(layoutInflater).apply {
             setContentView(this.root)
         }
